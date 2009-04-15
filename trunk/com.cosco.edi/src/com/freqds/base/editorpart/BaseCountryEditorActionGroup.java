869aca605676e3b29bd225717e3456a17422e83c
qd.freebsd <qd.freebsd@d4fc52ee-2984-11de-8f4a-8f4f67deb9c4>
2009-04-15 10:17:09 +0000

    修正增加、编辑界面

--- trunk/com.cosco.edi/src/com/freqds/base/editorpart/BaseCountryEditorActionGroup.java
+++ trunk/com.cosco.edi/src/com/freqds/base/editorpart/BaseCountryEditorActionGroup.java
@@ -1,10 +1,19 @@
 package com.freqds.base.editorpart;
 
+import java.sql.SQLException;
+import java.util.Date;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.wizard.WizardDialog;
 
 import com.freqds.base.dialog.BaseCountryDialog;
 import com.freqds.dbo.base.BaseCountryDAO;
+import com.freqds.mapping.base.BaseCountry;
 
+import cosco.vsagent.base.wizard.BasVslMsgWizard;
+import cosco.vsagent.mapping.base.Basvslmsg;
 import cosco.vsagent.system.EditorActionGroup;
 
 public class BaseCountryEditorActionGroup extends EditorActionGroup{
@@ -14,7 +23,26 @@ public class BaseCountryEditorActionGroup extends EditorActionGroup{
 		// TODO Auto-generated constructor stub
 	}
 	public void fireAddAction() {
-		new BaseCountryDialog(null).open();
+		BaseCountryDialog dialog = new BaseCountryDialog(null);
+		BaseCountry country = new BaseCountry();
+		dialog.setCountry(country);
+		if(dialog.open() == IDialogConstants.OK_ID){
+			country = dialog.getCountry();
+			Object params[] =new Object[4];
+			params[0] = country.getCountry_code();
+			params[1] = country.getCountry_cname();
+			params[2] = country.getCountry_ename();
+			params[3] = country.getInsert_usercode();
+			//params[4] = new Date();
+			String sql = "insert base_country(country_code,country_cname,country_ename,insert_usercode)values(?,?,?,?)";
+			BaseCountryDAO dao = new BaseCountryDAO();
+			try {
+				dao.update(sql, params);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public void fireFirstAction(){
@@ -28,5 +56,39 @@ public class BaseCountryEditorActionGroup extends EditorActionGroup{
 		getQueryInfo().currentPage =1;
 		tv.setInput( new BaseCountryDAO().getBaseCountry(getQueryInfo()));
 		refreshActionsState();
+	}
+	
+	public void fireModifyAction() {
+		IStructuredSelection sel  = (IStructuredSelection) tv.getSelection();
+		BaseCountry country = (BaseCountry)sel.getFirstElement();
+		
+		if(country == null) return;
+
+		BaseCountryDialog dialog = new BaseCountryDialog(null);
+		dialog.setCountry(country);
+		
+		if(dialog.open() == IDialogConstants.OK_ID){
+			country = dialog.getCountry();
+			BaseCountryDAO dao = new BaseCountryDAO();
+			Object params[] =new Object[3];
+			try {
+				/*
+				private String act;
+				private String memo;
+				private String ;
+				private Date ;
+				*/
+				params[0] = country.getCountry_cname();
+				params[1] = country.getCountry_ename();
+				params[2] = country.getCountry_code();
+				
+				dao.update("update base_country set country_cname =?,country_ename=? where country_code=?", params);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			dao.modifyBaseCountry(dialog.getCountry());
+			tv.refresh(country);
+		}
 	}
 }
