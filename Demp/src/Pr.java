ca4e3950b9237a1d6b29c084add4664e3775038f
qd.freebsd <qd.freebsd@d4fc52ee-2984-11de-8f4a-8f4f67deb9c4>
2009-04-15 14:31:01 +0000

    jjjjjjjjjjj

--- Demp/src/Pr.java
+++ Demp/src/Pr.java
@@ -7,6 +7,7 @@ public class Pr {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("==================");
+		System.out.println("==================");
 
 	}
 
